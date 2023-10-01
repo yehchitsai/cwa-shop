9ccd92abc6c435cbfb1e99110acf1bd9d6fe76e4
Yusong Hsu <sky172839465@gmail.com>
2023-10-01 20:33:29 +0800

    chore: add env file

--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,52 @@
 import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { viteMockServe } from 'vite-plugin-mock'
 import { version, name } from './package.json'
 
 const root = resolve(__dirname, 'src')
 const outDir = resolve(__dirname, 'dist')
+const envDir = resolve(__dirname, 'environments')
 const endpointFileName = 'index.html'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  define: {
-    'window.APP_VERSION': `"${version}"`,
-    'window.APP_BASENAME': `"${process.env.BASENAME ? `/${name}` : ''}"`
-  },
-  root,
-  plugins: [
-    react(),
-    viteMockServe({
-      mockPath: 'mock',
-      localEnabled: !!process.env.MOCK
-    })
-  ],
-  build: {
-    outDir,
-    emptyOutDir: true,
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: resolve(root, endpointFileName)
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, envDir) }
+  const viteConfig = {
+    base: './',
+    envDir,
+    define: {
+      'window.APP_VERSION': `"${version}"`,
+      'window.APP_BASENAME': `"${process.env.BASENAME ? `/${name}` : ''}"`
+    },
+    root,
+    plugins: [
+      react(),
+      viteMockServe({
+        mockPath: 'mock',
+        localEnabled: !!process.env.MOCK
+      })
+    ],
+    build: {
+      outDir,
+      emptyOutDir: true,
+      assetsDir: 'assets',
+      rollupOptions: {
+        input: {
+          main: resolve(root, endpointFileName)
+        }
       }
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://jsonplaceholder.typicode.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_MOCK_API_HOST,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
-})
+  return defineConfig(viteConfig)
+}
