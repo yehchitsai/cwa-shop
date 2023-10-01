e46dc1c87f1b8bc35e5b0bec683afc0698a279d4
Yusong Hsu <sky172839465@gmail.com>
2023-10-01 09:16:10 +0800

    feat: update mock data and card layout

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,14 +1,32 @@
-const Card = () => {
-  const imgUrl = new URL('./img.jpg', import.meta.url).href
+const Card = (props) => {
+  const {
+    item: {
+      id, images, type, price
+    } = {}
+  } = props
+  const imgUrl = new URL(`./${images[0]}`, import.meta.url).href
   return (
     <div className='p-4 max-xl:w-1/2 max-sm:w-full xl:w-1/3'>
       <div className='card bg-base-100 shadow-xl'>
         <figure><img src={imgUrl} alt='Shoes' /></figure>
         <div className='card-body'>
-          <h2 className='card-title'>Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <h2 className='card-title'>
+            <div className='flex flex-wrap'>
+              <div className='w-1/3'>
+                {`No.${id}`}
+              </div>
+              <div className='w-2/3 text-right'>
+                {`$${price} NTD`}
+              </div>
+              <div className='w-full'>
+                {type}
+              </div>
+            </div>
+          </h2>
           <div className='card-actions justify-end'>
-            <button type='button' className='btn btn-primary'>Buy Now</button>
+            <button type='button' className='btn btn-primary btn-outline'>
+              add to cart
+            </button>
           </div>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,23 @@ import { Link } from 'react-router-dom'
 import { times, random } from 'lodash-es'
 import Card from '../components/Card'
 
+const images = times(4, (index) => `img${index}.jpg`)
+const fishData = times(random(20, 40), (index) => {
+  const fishType = random(0, 3)
+  return {
+    index,
+    id: index * random(10, 20),
+    images: [images[fishType]],
+    type: `Fish ${fishType}`,
+    price: random(1000, 1500) * (index + 1)
+  }
+})
+
 const Home = () => (
   <>
     <div className='flex flex-wrap'>
-      {times(random(5, 15), (index) => (
-        <Card key={index} />
+      {fishData.map((item) => (
+        <Card key={item.id} item={item} />
       ))}
     </div>
     <br />
