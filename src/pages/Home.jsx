4dcf301497f93869900c0b29136eaf916d2920ea
Yusong Hsu <sky172839465@gmail.com>
2023-10-02 12:09:02 +0800

    refactor: using drawer only necessary

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,49 @@
 import { Link } from 'react-router-dom'
-import { times, random } from 'lodash-es'
+import { MdShoppingCart } from 'react-icons/md'
 import Card from '../components/Card'
 import ProductModel from '../components/Model/Product'
-
-const images = times(4, (index) => `img${index}.jpg`)
-const fishData = times(random(20, 40), (index) => {
-  const fishType = random(0, 3)
-  return {
-    index,
-    id: index * random(10, 20),
-    images: [images[fishType]],
-    type: `Fish ${fishType}`,
-    price: random(1000, 1500) * (index + 1)
-  }
-})
+import useFishData from '../hooks/useFishData'
+import SkeletonHome from '../components/Skeleton/Home'
+import Drawer from '../components/Drawer'
 
 const Home = () => {
+  const { data, isLoading } = useFishData()
+  if (isLoading) {
+    return <SkeletonHome />
+  }
+
   const openProductModal = () => document.querySelector('#productModel').showModal()
   return (
-    <>
-      <div className='flex flex-wrap'>
-        {fishData.map((item) => (
-          <Card
-            key={`${item.id}${item.price}`}
-            item={item}
-            onImageClick={openProductModal}
-          />
-        ))}
+    <Drawer
+      id='rootSidebar'
+      items={(
+        <>
+          <li><span>Sidebar Item 1</span></li>
+          <li><span>Sidebar Item 2</span></li>
+        </>
+      )}
+      openIcon={MdShoppingCart}
+      overlay
+      isRoot
+      rwd
+    >
+      <div className='max-lg:m-auto max-lg:max-w-2xl max-sm:min-w-full max-sm:p-4 sm:p-12 lg:max-w-5xl'>
+        <Link to='/about'>about</Link>
+        &nbsp;
+        <Link to='/detail'>detail</Link>
+        <br />
+        <div className='flex flex-wrap'>
+          {data.map((item) => (
+            <Card
+              key={`${item.id}${item.price}`}
+              item={item}
+              onImageClick={openProductModal}
+            />
+          ))}
+        </div>
       </div>
       <ProductModel id='productModel' />
-      <br />
-      <Link to='/about'>about</Link>
-      <br />
-      <Link to='/detail'>detail</Link>
-    </>
+    </Drawer>
   )
 }
 
