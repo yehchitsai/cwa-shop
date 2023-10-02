2446e0118a59d18286c3aadb6bf3f0c3902f6615
Yusong Hsu <sky172839465@gmail.com>
2023-10-02 18:59:35 +0800

    feat: update version position

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -49,7 +49,7 @@ const NavBar = (props) => {
               </ul>
             </details>
           </li>
-          <li><span className='tooltip' data-tip={window.APP_VERSION}>User 1</span></li>
+          <li><span className='tooltip tooltip-bottom' data-tip={`v${window.APP_VERSION}`}>User 1</span></li>
           <li><span><MdLogout size='1.5em' /></span></li>
         </ul>
       </div>
