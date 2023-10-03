472ac30b19a1de7d652bf33bf240486da61df0cd
Yusong Hsu <sky172839465@gmail.com>
2023-10-04 00:05:51 +0800

    feat: get fish info using useSWRMutation only trigger when open modal

--- src/hooks/useFishInfo.js
+++ src/hooks/useFishInfo.js
@@ -1,17 +1,21 @@
-import useSWR from 'swr'
+import useSWRMutation from 'swr/mutation'
 import qs from 'query-string'
 import { isEmpty } from 'lodash-es'
 
 const useFishInfo = (itemSerial) => {
   const params = { itemSerial }
   const url = `/v1/battafish?${qs.stringify(params)}`
-  const { data = [], error, isLoading } = useSWR(() => {
+  const {
+    data = [], error, isLoading, isMutating, trigger
+  } = useSWRMutation(() => {
     return isEmpty(itemSerial) ? null : url
-  })
+  }, { keepPreviousData: false })
   return {
     data,
     isLoading,
-    isError: error
+    isMutating,
+    isError: error,
+    trigger
   }
 }
 
