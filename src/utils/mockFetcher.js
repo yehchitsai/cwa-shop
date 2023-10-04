2be4e5f267a4049bd3bb5c8efab497cb75f23846
Yusong Hsu <sky172839465@gmail.com>
2023-10-05 02:07:42 +0800

    refactor: add dynamic import mock data

--- src/utils/mockFetcher.js
+++ src/utils/mockFetcher.js
@@ -1,45 +1,21 @@
 import qs from 'query-string'
 import {
-  find, delay
+  find, delay, flow, concat, values
 } from 'lodash-es'
-// import {
-//   find, delay, isEmpty, flow, concat, keys
-// } from 'lodash-es'
-import types from '../mock/battaFish/types'
-import users from '../mock/jsonplaceholder/users'
-import posts from '../mock/jsonplaceholder/posts'
 
-// let cachedMockData = []
-// const getMockData = async () => {
-//   if (!isEmpty(cachedMockData)) {
-//     return cachedMockData
-//   }
-//   const data = await flow(
-//     () => import.meta.glob('../mock/**/*.js'),
-//     async (mockFiles) => {
-//       const paths = keys(mockFiles)
-//       console.log({ mockFiles, paths })
-//       return Promise
-//         .all(paths.map((path) => {
-//           const fileUrl = new URL(path, import.meta.url).href
-//           console.log(fileUrl)
-//           return import(/* @vite-ignore */path)
-//         }))
-//         .then((modules) => concat(...modules.map((module) => module.default)))
-//     }
-//   )()
-//   cachedMockData = data
-//   return data
-// }
+const mockData = flow(
+  () => import.meta.glob('../mock/**/*.js', { eager: true }),
+  values,
+  (modules) => concat(...modules.map((module) => module.default))
+)()
 
 const mockFetcher = async (args) => {
   const { url: key } = args
-  // await getMockData()
-  const mockData = [...types, ...users, ...posts]
-  // console.log(mockData)
   const [endpoint, queryString] = key.split('?')
-  const mock = find(mockData, (item) => item.url.includes(endpoint)) || {}
-  const { response, timeout } = mock
+  const {
+    response = () => [],
+    timeout = 0
+  } = find(mockData, (item) => item.url.includes(endpoint)) || {}
   const resp = new Promise((resolve) => {
     delay(() => {
       const data = response({ query: qs.parse(queryString) })
