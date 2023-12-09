7118881662832fefb9ba3bfe9ccd8ea92677d9b9
Yusong Hsu <sky172839465@gmail.com>
2023-12-09 13:37:28 +0800

    feat: add login page dev env only

--- src/sites/index.jsx
+++ src/sites/index.jsx
@@ -4,7 +4,13 @@ import Root from '../components/Root'
 
 const links = flow(
   () => keys(import.meta.glob('./**/index.html')),
-  (paths) => paths.filter((path) => path !== './index.html'),
+  (paths) => paths.filter((path) => {
+    return (
+      path !== './index.html' &&
+      // login 只有在 mock 環境露出
+      (window.IS_MOCK ? true : !path.includes('./login'))
+    )
+  }),
   (filteredPaths) => filteredPaths.map((path) => path.replace('./', '/').replace('index.html', '')),
   (endpoints) => endpoints.map((endpoint) => ({ url: `${window.APP_BASENAME}${endpoint}`, name: endpoint.replace(/\//g, '') }))
 )()
---
+++ src/sites/login/pages/index.jsx
@@ -0,0 +1,51 @@
+import { Formik, Field, Form } from 'formik'
+import qs from 'query-string'
+
+const FORM = {
+  TOKEN_TYPE: 'token_type',
+  ACCESS_TOKEN: 'access_token'
+}
+
+const Login = () => {
+  return (
+    <div className='hero min-h-screen bg-base-200'>
+      <div className='hero-content flex-col lg:flex-row-reverse'>
+        <div className='card w-full max-w-sm shrink-0 bg-base-100 shadow-2xl'>
+          <Formik
+            initialValues={{
+              [FORM.TOKEN_TYPE]: 'Bearer',
+              [FORM.ACCESS_TOKEN]: 'mock-access-token_123'
+            }}
+          >
+            {({ values }) => (
+              <Form className='card-body'>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Token Type</span>
+                  </label>
+                  <Field name={FORM.TOKEN_TYPE} className='input input-bordered' required />
+                </div>
+                <div className='form-control'>
+                  <label className='label'>
+                    <span className='label-text'>Access Token</span>
+                  </label>
+                  <Field name={FORM.ACCESS_TOKEN} className='input input-bordered' required />
+                </div>
+                <div className='form-control mt-6'>
+                  <a
+                    className='btn btn-primary'
+                    href={window.location.href.replace(window.location.pathname, `${window.APP_BASENAME}/external/?${qs.stringify(values)}`)}
+                  >
+                    Login
+                  </a>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
