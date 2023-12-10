170ed31e9889aec84b0a6286633c6a2ca0d5b9a7
Yusong Hsu <sky172839465@gmail.com>
2023-12-10 19:04:31 +0800

    feat: add single & all remove with api

--- src/sites/external/pages/confirm/index.jsx
+++ src/sites/external/pages/confirm/index.jsx
@@ -1,30 +1,41 @@
 import { useState } from 'react'
-// import { Link } from 'react-router-dom'
 import { useRecoilState } from 'recoil'
 import { useTranslation } from 'react-i18next'
 import { MdDelete } from 'react-icons/md'
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData, useNavigate } from 'react-router-dom'
+import toast from 'react-hot-toast'
+import safeAwait from 'safe-await'
 import {
-  flow, get, size, sumBy, groupBy, reduce
+  flow, get, size, sumBy, groupBy, reduce, map, find
 } from 'lodash-es'
 import {
   selectedProductsState
 } from '../../../../state/selectedProducts'
+import getApiHost from '../../../../utils/getApiHost'
 import useFishTypes from '../../../../hooks/useFishTypes'
 import useOnInit from '../../../../hooks/useOnInit'
+import useCreate from '../../../../hooks/useCreate'
 import SkeletonHome from '../../../../components/Skeleton/Home'
 import LazyImage from '../../../../components/LazyImage'
 import ProductModel from '../../../../components/Model/Product'
 
+const preOrderHost = getApiHost('VITE_AWS_FISH_PREORDER')
+const preOrderEndPoint = `${import.meta.env.VITE_AWS_HOST_PREFIX}/bettafishpreorder`
+// const orderHost = getApiHost('VITE_AWS_FISH_ORDER')
+// const orderEndPoint = `${import.meta.env.VITE_AWS_HOST_PREFIX}/fishorder`
+
 const productModelKey = 'productModel'
 
 const Confirm = () => {
+  const navigate = useNavigate()
   const { t, i18n } = useTranslation()
   const { fishTypeMap, isLoading } = useFishTypes(i18n.language)
   const [selectedProducts, setSelectedProducts] = useRecoilState(selectedProductsState)
   const [targetProduct, setTargetProduct] = useState({})
   const [isProductModalOpen, setIsProductModalOpen] = useState(false)
   const defaultSelectedProducts = useLoaderData()
+  const { trigger: reserveByItemSerial, isMutating: isReserving } = useCreate(preOrderHost)
+  // const { trigger: orderByItemSerial, isMutating: isOrdering } = useCreate(orderHost)
   const selectedTypes = flow(
     () => groupBy(selectedProducts, (selectedProduct) => selectedProduct.fishType),
     (groupedProducts) => reduce(groupedProducts, (data, products, fishType) => {
@@ -44,15 +55,58 @@ const Confirm = () => {
 
   const closeProductModal = () => setIsProductModalOpen(false)
 
-  const onRemove = (product) => () => {
-    const newSelectedProducts = selectedProducts.filter((selectedProduct) => {
-      return selectedProduct.itemSerial !== product.itemSerial
-    })
-    setSelectedProducts(newSelectedProducts)
+  const onRemove = (product) => async () => {
+    const toastId = toast.loading('Updating...')
+    const targetItemSerial = get(product, 'itemSerial')
+    const { add, remove } = selectedProducts.reduce((collect, selectedProduct) => {
+      const isRemoveTarget = selectedProduct.itemSerial === targetItemSerial
+      if (isRemoveTarget) {
+        collect.remove.push(selectedProduct)
+      } else {
+        collect.add.push(selectedProduct)
+      }
+      return collect
+    }, { add: [], remove: [] })
+    const reserveItemSerials = map(add, 'itemSerial')
+    const clearItemSerials = map(remove, 'itemSerial')
+    const fishType = get(remove, '0.fishType')
+    const [reserveError, reserveData] = await safeAwait(reserveByItemSerial({
+      url: preOrderEndPoint,
+      fishType,
+      reserveItemSerials,
+      clearItemSerials
+    }))
+    if (reserveError) {
+      toast.error(`Error! No.${targetItemSerial} ${reserveError.message}`, { id: toastId })
+      return
+    }
+
+    setSelectedProducts(add)
+    const { reason = 'unknow update status' } = flow(
+      () => get(reserveData, 'results'),
+      (results) => find(results, { itemSerial: targetItemSerial }) || {}
+    )()
+    toast.success(`No.${targetItemSerial} ${reason}`, { id: toastId })
   }
 
-  const onRemoveAll = () => {
+  const onRemoveAll = async () => {
+    const toastId = toast.loading('Updating...')
+    const fishType = get(selectedProducts, '0.fishType')
+    const clearItemSerials = map(selectedProducts, 'itemSerial')
+    const [reserveError] = await safeAwait(reserveByItemSerial({
+      url: preOrderEndPoint,
+      fishType,
+      reserveItemSerials: [],
+      clearItemSerials
+    }))
+    if (reserveError) {
+      toast.error(`Error! ${reserveError.message}`, { id: toastId })
+      return
+    }
+
     setSelectedProducts([])
+    toast.success('All items removed,\nback to products page in 3sec', { id: toastId })
+    setTimeout(() => navigate('../'), 3000)
   }
 
   useOnInit(() => {
@@ -88,7 +142,7 @@ const Confirm = () => {
               } = selectedProduct
               const { fishName } = get(fishTypeMap, fishType, {})
               return (
-                <tr key={itemSerial}>
+                <tr key={index}>
                   <th>{index + 1}</th>
                   <th>
                     <LazyImage
@@ -107,6 +161,7 @@ const Confirm = () => {
                       type='button'
                       className='btn btn-square btn-error btn-outline'
                       onClick={onRemove(selectedProduct)}
+                      disabled={isReserving}
                     >
                       <MdDelete size='1.5em' />
                     </button>
@@ -127,8 +182,8 @@ const Confirm = () => {
             <tr>
               <th colSpan={2}>{`${size(selectedProducts)}`}</th>
               <td colSpan={2}>
-                {selectedTypes.map(({ fishName, count }) => (
-                  <p key={fishName}>{`${fishName} x ${count}`}</p>
+                {selectedTypes.map(({ fishName, count }, index) => (
+                  <p key={index}>{`${fishName} x ${count}`}</p>
                 ))}
               </td>
               <td>
