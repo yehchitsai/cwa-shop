190602c0ee0f2d23de7811edd4e2e3d0c9173eba
Yusong Hsu <sky172839465@gmail.com>
2023-12-12 23:10:43 +0800

    fix: miss handle error image url

--- src/components/LazyImage/index.jsx
+++ src/components/LazyImage/index.jsx
@@ -1,26 +1,49 @@
 import { Suspense } from 'react'
-import { isEmpty } from 'lodash-es'
+import { get, isEmpty } from 'lodash-es'
 import { useImage } from 'react-image'
 import Skeleton from 'react-loading-skeleton'
 import { ErrorBoundary } from 'react-error-boundary'
 
 const fallbackRender = (props) => {
   const { error } = props
+  console.log(error)
+
   return (
     <div className='block w-full bg-black py-10 text-center text-white'>
-      <pre>{error.message}</pre>
+      {get(error, 'message', 'Error')}
     </div>
   )
 }
 
-const Image = (props) => {
+const ReactImage = (props) => {
   const {
-    src: imageSrc, alt, style, className, loaderClassName, onClick
+    src: imageSrc, alt, className, onClick
   } = props
   const { src } = useImage({
     srcList: imageSrc
   })
 
+  return (
+    <img
+      src={src}
+      alt={alt}
+      // crossOrigin='anonymous'
+      loading='lazy'
+      className={className}
+      onClick={onClick}
+    />
+  )
+}
+
+const LazyImage = (props) => {
+  const { src, style, loaderClassName } = props
+  if (isEmpty(src)) {
+    return (
+      <div className='block w-full bg-black py-10 text-center text-white'>
+        No Image
+      </div>
+    )
+  }
   return (
     <ErrorBoundary fallbackRender={fallbackRender}>
       <Suspense
@@ -31,30 +54,9 @@ const Image = (props) => {
           />
         )}
       >
-        <img
-          src={src}
-          alt={alt}
-          crossOrigin='anonymous'
-          loading='lazy'
-          className={className}
-          onClick={onClick}
-        />
+        <ReactImage {...props} />
       </Suspense>
     </ErrorBoundary>
-  )
-}
-
-const LazyImage = (props) => {
-  const { src } = props
-  if (isEmpty(src)) {
-    return (
-      <div className='block w-full bg-black py-10 text-center text-white'>
-        No Image
-      </div>
-    )
-  }
-  return (
-    <Image {...props} />
   )
 }
 
