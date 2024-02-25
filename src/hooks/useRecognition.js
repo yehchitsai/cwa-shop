42da1eb4ffde74823b1c32a078ec5e971c17a9b7
Yusong Hsu <sky172839465@gmail.com>
2024-02-25 15:01:34 +0800

    feat: add upload video apis

---
+++ src/hooks/useRecognition.js
@@ -0,0 +1,89 @@
+import { useState, useRef } from 'react'
+import safeAwait from 'safe-await'
+import qs from 'query-string'
+import { get } from 'lodash-es'
+import getApiHost from '../utils/getApiHost'
+import retry from '../utils/retry'
+import useOnInit from './useOnInit'
+import useUpdate from './useUpdate'
+import useGet from './useGet'
+
+const putVideHost = getApiHost('VITE_AWS_PUT_VIDEO')
+const getVideoRecognitionHost = getApiHost('VITE_AWS_GET_VIDEO_RECOGNITION')
+const awsHostPrefix = import.meta.env.VITE_AWS_HOST_PREFIX
+
+const retryAction = async (action) => {
+  const checker = (result) => {
+    const recognitionStatus = get(result, 'status')
+    return recognitionStatus === 'success'
+  }
+
+  return retry('recognition failed')(action, checker, 1)
+}
+
+const useRecognition = (file, onSuccess) => {
+  const isInit = useRef(false)
+  const [isLoading, setIsLoading] = useState(true)
+  const [recognitionError, setRecognitionError] = useState(null)
+  const [status, setStatus] = useState('')
+  const [data, setData] = useState({})
+  const { name: fileName, url } = file
+  const {
+    trigger: uploadVideo
+  } = useUpdate(putVideHost)
+  const {
+    trigger: getVideoRecognition
+  } = useGet(getVideoRecognitionHost)
+
+  const recognition = async () => {
+    setIsLoading(true)
+    setStatus('loading')
+    const [uploadVideoError] = await safeAwait(
+      uploadVideo({ url: `/v1/ithomebucket/${fileName}`, [fileName]: url })
+    )
+    if (uploadVideoError) {
+      setIsLoading(false)
+      setRecognitionError(uploadVideoError)
+      setStatus('fail')
+      return
+    }
+
+    const params = { file: fileName }
+    const getRecognitionUrl = `${awsHostPrefix}/getRecognition?${qs.stringify(params)}`
+    const [videoRecognitionError, result] = await safeAwait(
+      retryAction(() => getVideoRecognition({ url: getRecognitionUrl }))
+    )
+    if (videoRecognitionError) {
+      setIsLoading(false)
+      setRecognitionError(videoRecognitionError)
+      setStatus('fail')
+      return
+    }
+
+    setIsLoading(false)
+    const newData = get(result, 'results', {})
+    setData(newData)
+    onSuccess(newData)
+    setStatus('success')
+  }
+
+  useOnInit(() => {
+    if (isInit.current) {
+      return
+    }
+
+    const trigger = () => recognition()
+    trigger()
+    isInit.current = true
+  })
+
+  return {
+    trigger: recognition,
+    isLoading,
+    error: recognitionError,
+    status,
+    data
+  }
+}
+
+export default useRecognition
---
+++ src/mock/battaFish/upload.js
@@ -0,0 +1,88 @@
+import { flow, random, times } from 'lodash-es'
+import getApiPrefix from '../../utils/getApiPrefix'
+
+const videos = [
+  'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'
+]
+
+const getFakeImage = (size, text) => {
+  return `https://fakeimg.pl/${size}x${size * 3}/?text=${text}&font=lobster&font_size=50`
+}
+
+const RECOGNITION_STATUS = {
+  SUCCESS: 'success',
+  PENDING: 'pending',
+  FAIL: 'fail'
+}
+
+const RECOGNITION_STATUS_LIST = [
+  RECOGNITION_STATUS.PENDING,
+  RECOGNITION_STATUS.FAIL,
+  RECOGNITION_STATUS.FAIL,
+  RECOGNITION_STATUS.SUCCESS,
+  RECOGNITION_STATUS.FAIL,
+  RECOGNITION_STATUS.SUCCESS,
+  RECOGNITION_STATUS.PENDING,
+  RECOGNITION_STATUS.FAIL,
+  RECOGNITION_STATUS.FAIL,
+  RECOGNITION_STATUS.SUCCESS,
+  RECOGNITION_STATUS.FAIL
+]
+
+const resultError = (message = 'Request failed', { code = 0, result = null } = {}) => {
+  return {
+    code,
+    result,
+    message,
+    type: 'error'
+  }
+}
+
+export default [
+  {
+    url: '/v1/ithomebucket/:file',
+    method: 'put',
+    timeout: 1500,
+    response: ({ query }) => console.log(query)
+  },
+  {
+    url: `${getApiPrefix()}/getRecognition`,
+    method: 'get',
+    timeout: 1500,
+    response: ({ query: stringObject }) => {
+      const {
+        file
+      } = JSON.parse(JSON.stringify(stringObject))
+      const status = RECOGNITION_STATUS_LIST[random(0, RECOGNITION_STATUS_LIST.length - 1)]
+      if (status === RECOGNITION_STATUS.PENDING) {
+        return { status }
+      }
+
+      if (status === RECOGNITION_STATUS.FAIL) {
+        return {
+          status,
+          ...resultError()
+        }
+      }
+
+      const results = {
+        itemVideos: [videos[random(0, 2)]],
+        fishType: `FF${random(1000, 2000)}L`,
+        itemSerial: `${random(100, 1000)}`,
+        itemImages: flow(
+          () => random(2, 3),
+          (count) => times(count, (index) => {
+            const size = 200 * (index + 1)
+            return getFakeImage(size, `${file} ${index}`)
+          })
+        )()
+      }
+      return {
+        status,
+        results
+      }
+    }
+  }
+]
