720d44c298e65fe19ceaef97c29320a6d1eab6ae
Yusong Hsu <sky172839465@gmail.com>
2024-03-09 20:52:59 +0800

    chore: update message display

--- src/utils/retry.js
+++ src/utils/retry.js
@@ -1,7 +1,11 @@
 import safeAwait from 'safe-await'
 import { get } from 'lodash-es'
 
-const retry = (errorMessage) => async (action, checker, times = 10) => {
+const defaultGetErrorMessage = ({ error }) => {
+  return get(error, 'message', error)
+}
+
+const retry = (getMessage = defaultGetErrorMessage) => async (action, checker, times = 10) => {
   const [error, result] = await safeAwait(action())
   const isExpectResult = checker(result)
   if (times === 0 || isExpectResult) {
@@ -9,13 +13,13 @@ const retry = (errorMessage) => async (action, checker, times = 10) => {
       return result
     }
 
-    console.log(error)
-    throw new Error(errorMessage || get(error, 'message', error))
+    console.log(error, result)
+    throw new Error(getMessage({ error, result }))
   }
 
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(retry(errorMessage)(action, checker, times - 1))
+      resolve(retry(getMessage)(action, checker, times - 1))
     }, 3000)
   })
 }
