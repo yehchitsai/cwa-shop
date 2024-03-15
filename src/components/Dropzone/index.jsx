9b1f248d91bb5b63ba5509e6758823124c35d8a7
Yusong Hsu <sky172839465@gmail.com>
2024-03-15 20:46:24 +0800

    feat: prevent drop same name files

--- src/components/Dropzone/index.jsx
+++ src/components/Dropzone/index.jsx
@@ -44,19 +44,28 @@ const Dropzone = (props) => {
     setSelectType(newSelectType)
   }
 
-  const onDrop = useCallback(async (acceptedFiles, defaultFileRejections) => {
+  const onDrop = useCallback(async (defaultAcceptedFiles, defaultFileRejections) => {
     setIsPending(true)
+    const acceptFilesMap = keyBy(files, 'name')
+    const acceptedFiles = filter(defaultAcceptedFiles, (acceptedFile) => {
+      return !(acceptedFile.name in acceptFilesMap)
+    })
     const fileRejections = isSelectFolder
       // select folder will ignore unaccept type file
       ? filter(defaultFileRejections, (rejection) => {
-        return (rejection.file.type in accept) && !rejection.file.name.startsWith('.')
+        const fileName = get(rejection, 'file.name')
+        return (
+          (rejection.file.type in accept) &&
+          !fileName.startsWith('.') &&
+          !(fileName in acceptFilesMap)
+        )
       })
       : defaultFileRejections
 
     if (isEmpty(acceptedFiles)) {
       setFieldValue(rejectField, fileRejections)
       setIsPending(false)
-      onFinish([])
+      onFinish(files)
       return
     }
 
