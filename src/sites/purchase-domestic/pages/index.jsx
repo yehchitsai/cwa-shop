8d9562c5b462cb305e436abb60b65c5cb185bec4
Yusong Hsu <sky172839465@gmail.com>
2024-06-09 13:58:21 +0800

    feat: add click add to cart tip

--- src/sites/purchase-domestic/pages/index.jsx
+++ src/sites/purchase-domestic/pages/index.jsx
@@ -1,6 +1,7 @@
 import { useSearchParams } from 'react-router-dom'
 import clx from 'classnames'
 import { MdShoppingCart, MdSearch } from 'react-icons/md'
+import { GiClick } from 'react-icons/gi'
 import { size, times } from 'lodash-es'
 import CartBottomItems from '../../../components/CartBottomItems'
 import CartItems from '../../../components/CartItems'
@@ -57,59 +58,70 @@ const PurchaseDomestic = () => {
       indicator={size(selectProducts)}
       overlay
     >
-      <div className='grid grid-flow-col gap-4 p-4'>
-        <div className='grid-cols-1'>
-          <ItemSelectSection />
+      <div className='space-y-4 p-4'>
+        <div className='grid grid-flow-col gap-4'>
+          <div className='grid-cols-1'>
+            <ItemSelectSection />
+          </div>
+          <div className='grid-cols-1'>
+            <label className='input input-bordered flex items-center'>
+              <input type='text' className='grow' placeholder='Search' autoComplete='off' />
+              <MdSearch size='1.5em' />
+            </label>
+          </div>
         </div>
-        <div className='grid-cols-1'>
-          <label className='input input-bordered flex items-center'>
-            <input type='text' className='grow' placeholder='Search' autoComplete='off' />
-            <MdSearch size='1.5em' />
-          </label>
+        <p className='flex gap-2 text-indigo-500'>
+          <GiClick size='1.5em' className='!fill-indigo-500' />
+          點擊列加入購物車
+        </p>
+        <div className='overflow-x-auto'>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>項次</th>
+                <th>品名</th>
+                <th>尺寸</th>
+                <th>單價</th>
+                <th>建議零售價</th>
+                <th>在庫量</th>
+                <th>起購量</th>
+                <th>說明</th>
+                <th>特殊要求</th>
+                <th>購買數量</th>
+                <th>金額</th>
+                <th>圖片連結</th>
+                <th>影片連結</th>
+              </tr>
+            </thead>
+            <tbody>
+              {times(50, (index) => {
+                return (
+                  <tr
+                    key={index}
+                    className={clx(
+                      'whitespace-nowrap',
+                      { 'bg-gray-500 text-white': index % 5 === 0 }
+                    )}
+                  >
+                    <td className='text-center'>{index + 1}</td>
+                    <td>{`品名${index}`}</td>
+                    <td>{`尺寸${index}`}</td>
+                    <td>{`單價${index}`}</td>
+                    <td>{`建議零售價${index}`}</td>
+                    <td>{`在庫量${index}`}</td>
+                    <td>{`起購量${index}`}</td>
+                    <td>{`說明${index}`}</td>
+                    <td>{`特殊要求${index}`}</td>
+                    <td>{`購買數量${index}`}</td>
+                    <td>{`金額${index}`}</td>
+                    <td>{`圖片連結${index}`}</td>
+                    <td>{`影片連結${index}`}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
         </div>
-      </div>
-      <div className='overflow-x-auto px-4'>
-        <table className='table'>
-          {/* head */}
-          <thead>
-            <tr>
-              <th>項次</th>
-              <th>品名</th>
-              <th>尺寸</th>
-              <th>單價</th>
-              <th>建議零售價</th>
-              <th>在庫量</th>
-              <th>起購量</th>
-              <th>說明</th>
-              <th>特殊要求</th>
-              <th>購買數量</th>
-              <th>金額</th>
-              <th>圖片連結</th>
-              <th>影片連結</th>
-            </tr>
-          </thead>
-          <tbody>
-            {times(50, (index) => {
-              return (
-                <tr key={index} className='whitespace-nowrap'>
-                  <td>{index}</td>
-                  <td>{`品名${index}`}</td>
-                  <td>{`尺寸${index}`}</td>
-                  <td>{`單價${index}`}</td>
-                  <td>{`建議零售價${index}`}</td>
-                  <td>{`在庫量${index}`}</td>
-                  <td>{`起購量${index}`}</td>
-                  <td>{`說明${index}`}</td>
-                  <td>{`特殊要求${index}`}</td>
-                  <td>{`購買數量${index}`}</td>
-                  <td>{`金額${index}`}</td>
-                  <td>{`圖片連結${index}`}</td>
-                  <td>{`影片連結${index}`}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
       </div>
     </Drawer>
   )
