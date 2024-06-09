731963c3b54a5e41e374b704cbcf6eb02983183b
Yusong Hsu <sky172839465@gmail.com>
2024-06-09 13:58:49 +0800

    feat: add sticky table & selectable table row

--- src/sites/purchase-domestic/pages/index.jsx
+++ src/sites/purchase-domestic/pages/index.jsx
@@ -1,8 +1,9 @@
+import { useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import clx from 'classnames'
-import { MdShoppingCart, MdSearch } from 'react-icons/md'
+import { MdShoppingCart, MdSearch, MdOutlineDelete } from 'react-icons/md'
 import { GiClick } from 'react-icons/gi'
-import { size, times } from 'lodash-es'
+import { keyBy, size, times } from 'lodash-es'
 import CartBottomItems from '../../../components/CartBottomItems'
 import CartItems from '../../../components/CartItems'
 import Drawer from '../../../components/Drawer'
@@ -41,7 +42,29 @@ const ItemSelectSection = () => {
 }
 
 const PurchaseDomestic = () => {
-  const selectProducts = []
+  const [selectProducts, setSelectProducts] = useState([])
+  const selectProductMap = keyBy(selectProducts, 'id')
+
+  const onRemoveRow = (id) => {
+    const newSelectProducts = selectProducts.filter((selectProduct) => {
+      return selectProduct.id !== id
+    })
+    setSelectProducts(newSelectProducts)
+  }
+
+  const onSelectRow = (id) => {
+    setSelectProducts([...selectProducts, { id }])
+  }
+
+  const onClickRow = (id) => {
+    const isSelected = id in selectProductMap
+    if (isSelected) {
+      onRemoveRow(id)
+      return
+    }
+    onSelectRow(id)
+  }
+
   return (
     <Drawer
       id='rootSidebar'
@@ -70,40 +93,59 @@ const PurchaseDomestic = () => {
             </label>
           </div>
         </div>
-        <p className='flex gap-2 text-indigo-500'>
+        <p className='flex gap-2'>
           <GiClick size='1.5em' className='!fill-indigo-500' />
-          點擊列加入購物車
+          加入購物車
+          <MdOutlineDelete size='1.5em' className='!fill-red-500' />
+          從購物車移除
         </p>
-        <div className='overflow-x-auto'>
-          <table className='table'>
+        <div className='h-[calc(100dvh-12.5rem)] overflow-x-auto'>
+          <table className='table table-pin-rows table-pin-cols'>
             <thead>
               <tr>
                 <th>項次</th>
-                <th>品名</th>
-                <th>尺寸</th>
-                <th>單價</th>
-                <th>建議零售價</th>
-                <th>在庫量</th>
-                <th>起購量</th>
-                <th>說明</th>
-                <th>特殊要求</th>
-                <th>購買數量</th>
-                <th>金額</th>
-                <th>圖片連結</th>
-                <th>影片連結</th>
+                <td>品名</td>
+                <td>尺寸</td>
+                <td>單價</td>
+                <td>建議零售價</td>
+                <td>在庫量</td>
+                <td>起購量</td>
+                <td>說明</td>
+                <td>特殊要求</td>
+                <td>購買數量</td>
+                <td>金額</td>
+                <td>圖片連結</td>
+                <td>影片連結</td>
               </tr>
             </thead>
             <tbody>
               {times(50, (index) => {
+                const isSelected = index in selectProductMap
                 return (
                   <tr
                     key={index}
                     className={clx(
-                      'whitespace-nowrap',
-                      { 'bg-gray-500 text-white': index % 5 === 0 }
+                      'whitespace-nowrap cursor-pointer',
+                      { 'bg-base-200': isSelected }
                     )}
+                    onClick={() => onClickRow(index)}
                   >
-                    <td className='text-center'>{index + 1}</td>
+                    <th className={clx({ 'bg-base-200': isSelected })}>
+                      <label
+                        className={clx(
+                          'swap text-sm flex justify-center gap-2',
+                          { 'swap-active': isSelected }
+                        )}
+                      >
+                        <span className={clx('swap-on', { hidden: !isSelected })}>
+                          <MdOutlineDelete size='1.5em' className='!fill-red-500' />
+                        </span>
+                        <span className={clx('swap-off', { hidden: isSelected })}>
+                          <GiClick size='1.5em' className='!fill-indigo-500' />
+                        </span>
+                        {index + 1}
+                      </label>
+                    </th>
                     <td>{`品名${index}`}</td>
                     <td>{`尺寸${index}`}</td>
                     <td>{`單價${index}`}</td>
