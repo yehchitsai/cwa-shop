51abe6f630e6e0b24dadc04ceb2f633ae1cb231e
Yusong Hsu <sky172839465@gmail.com>
2024-06-14 22:02:55 +0800

    feat: add new mock api

--- src/mock/purchase/category.js
+++ src/mock/purchase/category.js
@@ -33,6 +33,27 @@ const getFakeImage = (width, height, text) => {
 
 export default [
   {
+    url: `${awsHostPrefix}/getcategorylist`,
+    method: 'get',
+    timeout: 100,
+    response: () => {
+      const category_list = times(random(5, 10), (index) => {
+        return {
+          category: `type_${index}`,
+          subcategory: times(random(5, 10), (subIndex) => `fish_name_${subIndex}`)
+        }
+      })
+      return {
+        status: 'success',
+        results: {
+          update_date: '2024-05-27',
+          delivery_date: '2024-06-01',
+          category_list
+        }
+      }
+    }
+  },
+  {
     url: `${awsHostPrefix}/categoryinfo`,
     method: 'get',
     timeout: 100,
@@ -66,6 +87,58 @@ export default [
     }
   },
   {
+    url: `${awsHostPrefix}/queryinfo`,
+    method: 'get',
+    timeout: 100,
+    response: ({ query: stringObject }) => {
+      const {
+        phrase
+      } = JSON.parse(JSON.stringify(stringObject))
+      const items = times(random(10, 30), (index) => {
+        const fishName = `fish_name＿${phrase}_${index}`
+        return {
+          fish_code: `FF120L${index}`,
+          science_name: `science_name_${phrase}_${index}`,
+          fish_name: fishName,
+          fish_size: FISH_SIZES[random(0, 2)],
+          unit_price: `${random(10, 100)}`,
+          retail_price: `${random(10, 100)}`,
+          inventory: `${random(10, 100)}`,
+          min_purchase_quantity: `${random(10, 20)}`,
+          note: ['', `note_${index}`][random(0, 1)],
+          image_link: getFakeImage(100, 100, `${phrase}-${fishName}`),
+          video_link: videos[random(0, 2)]
+        }
+      })
+      return {
+        status: 'success',
+        results: {
+          items
+        }
+      }
+    }
+  },
+  {
+    url: `${awsHostPrefix}/getexportcategorylist`,
+    method: 'get',
+    timeout: 100,
+    response: () => {
+      const category_list = times(random(5, 10), (index) => {
+        return {
+          category: `type_${index}`,
+          subcategory: times(random(5, 10), (subIndex) => `fish_name_${subIndex}`)
+        }
+      })
+      return {
+        status: 'success',
+        results: {
+          update_date: '2024-05-27',
+          category_list
+        }
+      }
+    }
+  },
+  {
     url: `${awsHostPrefix}/exportcategoryinfo`,
     method: 'get',
     timeout: 100,
@@ -87,6 +160,38 @@ export default [
           box_qty: `${random(10, 20)}`,
           note: ['', `note_${index}`][random(0, 1)],
           image_link: getFakeImage(100, 100, `${cat}-${fishName}`),
+          video_link: videos[random(0, 2)]
+        }
+      })
+      return {
+        status: 'success',
+        results: {
+          items
+        }
+      }
+    }
+  },
+  {
+    url: `${awsHostPrefix}/exportqueryinfo`,
+    method: 'get',
+    timeout: 100,
+    response: ({ query: stringObject }) => {
+      const {
+        phrase
+      } = JSON.parse(JSON.stringify(stringObject))
+      const items = times(random(10, 30), (index) => {
+        const fishName = `fish_name＿${phrase}_${index}`
+        return {
+          fish_code: `FF120L${index}`,
+          science_name: `science_name_${phrase}_${index}`,
+          fish_name: fishName,
+          fish_size: FISH_SIZES[random(0, 2)],
+          unit_price: `${random(10, 100)}`,
+          package_qty: `${random(10, 100)}`,
+          inventory: `${random(10, 100)}`,
+          box_qty: `${random(10, 20)}`,
+          note: ['', `note_${index}`][random(0, 1)],
+          image_link: getFakeImage(100, 100, `${phrase}-${fishName}`),
           video_link: videos[random(0, 2)]
         }
       })
