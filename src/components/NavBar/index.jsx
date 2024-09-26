6a28b18096537e3acfb9ff824d6efec7cc8be901
sky172839465 <sky172839465@gmail.com>
2024-09-27 06:52:22 +0800

    refactor: navbar remove detect site

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,95 +1,13 @@
-import { useLocation } from 'react-router-dom'
-import { useTranslation } from 'react-i18next'
 import clx from 'classnames'
 import Logo from './Logo'
-import LangsAction from './LangsAction'
-import LogoutAction from './LogoutAction'
-import UserAction from './UserAction'
-import PurchaseAction from './PurchaseAction'
-import getEntry from '../../utils/getEntry'
-
-const NAV_BAR_TYPE = {
-  SHOP: 'shop',
-  PURCHASE: 'purchase',
-  STAFF: 'staff'
-}
-
-const useNavBarType = () => {
-  const location = useLocation()
-  const { isShop, isPurchase, isStaff } = getEntry(location)
-
-  let navBarType
-  switch (true) {
-    case isPurchase: {
-      navBarType = NAV_BAR_TYPE.PURCHASE
-      break
-    }
-    case isStaff: {
-      navBarType = NAV_BAR_TYPE.STAFF
-      break
-    }
-    case isShop:
-    default: {
-      navBarType = NAV_BAR_TYPE.SHOP
-      break
-    }
-  }
-  return navBarType
-}
-
-const NavBarActions = (props) => {
-  const { fixed } = props
-  const navBarType = useNavBarType()
-
-  if (navBarType === NAV_BAR_TYPE.SHOP) {
-    return (
-      <>
-        <LangsAction />
-        <UserAction fixed={fixed} />
-        <LogoutAction />
-      </>
-    )
-  }
-
-  if (navBarType === NAV_BAR_TYPE.PURCHASE) {
-    return (
-      <>
-        <LangsAction />
-        <PurchaseAction />
-        <LogoutAction />
-      </>
-    )
-  }
-
-  if (navBarType === NAV_BAR_TYPE.STAFF) {
-    return (
-      <>
-        <LangsAction />
-        <LogoutAction />
-      </>
-    )
-  }
-
-  return null
-}
 
 const NavBar = (props) => {
-  const { fixed, appBaseName } = props
-  const { t } = useTranslation()
-  const location = useLocation()
-  const navBarType = useNavBarType()
-  const { isDev } = getEntry(location)
-  const isHiddenNavBar = isDev && location.pathname === '/'
-
-  if (isHiddenNavBar) {
-    return null
-  }
-
-  const LOGO_TITLE = {
-    [NAV_BAR_TYPE.SHOP]: t('shopLogoTitle'),
-    [NAV_BAR_TYPE.PURCHASE]: t('purchaseLogoTitle'),
-    [NAV_BAR_TYPE.STAFF]: t('purchaseLogoTitle')
-  }
+  const {
+    fixed,
+    appBaseName,
+    title,
+    actions
+  } = props
 
   return (
     <div
@@ -101,14 +19,12 @@ const NavBar = (props) => {
     >
       <div className='flex-1'>
         <Logo appBaseName={appBaseName}>
-          {LOGO_TITLE[navBarType]}
+          {title}
         </Logo>
       </div>
       <div className='flex flex-1 justify-end'>
         <div className='flex items-stretch'>
-          <NavBarActions
-            fixed={fixed}
-          />
+          {actions}
         </div>
       </div>
     </div>
