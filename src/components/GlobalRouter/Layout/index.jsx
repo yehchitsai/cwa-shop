82ce283e1728ce4b4933475acfe94453acae5208
sky172839465 <sky172839465@gmail.com>
2024-09-27 07:59:33 +0800

    feat: apply global router

--- src/components/GlobalRouter/Layout/index.jsx
+++ src/components/GlobalRouter/Layout/index.jsx
@@ -1,14 +1,19 @@
 import { Outlet, useLocation } from 'react-router-dom'
+import { get } from 'lodash-es'
 
-const layouts = import.meta.glob('../../../sites/**/layout/index.jsx')
-// const loaders = import.meta.glob('../sites/**/layout/index.loader.js')
-console.log(layouts)
+const layouts = import.meta.glob('../../../sites/**/layout/index.jsx', { eager: true })
 
-const Layout = () => {
-  const location = useLocation()
-  console.log(location.pathname)
+const Layout = (props) => {
+  const { pathname } = useLocation()
+  if (pathname === '/') {
+    return (
+      <Outlet />
+    )
+  }
+  const rootPathname = pathname.split('/')[1]
+  const Component = get(layouts, [`../../../sites/${rootPathname}/layout/index.jsx`, 'default'], Outlet)
   return (
-    <Outlet />
+    <Component {...props} />
   )
 }
 
