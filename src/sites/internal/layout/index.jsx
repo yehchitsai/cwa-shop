9b2179a3ca59e69844dc0970629a7ec3a249148a
sky172839465 <sky172839465@gmail.com>
2024-09-27 09:04:01 +0800

    chore: internal remove auth check

--- src/sites/internal/layout/index.jsx
+++ src/sites/internal/layout/index.jsx
@@ -2,9 +2,6 @@ import { Outlet } from 'react-router-dom'
 import i18n from '../../../i18n'
 import Root from '../../../components/Root'
 import NavBar from '../../../components/NavBar'
-import LangsAction from '../../../components/NavBar/LangsAction'
-import UserAction from '../../../components/NavBar/UserAction'
-import LogoutAction from '../../../components/NavBar/LogoutAction'
 
 const SiteLayout = (props) => {
   const { appBaseName } = props
@@ -13,13 +10,6 @@ const SiteLayout = (props) => {
       <NavBar
         appBaseName={appBaseName}
         title={i18n.t('shopLogoTitle')}
-        actions={(
-          <>
-            <LangsAction />
-            <UserAction fixed />
-            <LogoutAction />
-          </>
-        )}
       />
       <div className='w-full max-w-full'>
         <Outlet />
