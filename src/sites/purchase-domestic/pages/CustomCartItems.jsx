50b589ec6bfddaf9da363a4d5e491064158892eb
sky172839465 <sky172839465@gmail.com>
2024-10-15 21:52:16 +0800

    feat: using prepurchaseorder data

--- src/sites/purchase-domestic/pages/CustomCartItems.jsx
+++ src/sites/purchase-domestic/pages/CustomCartItems.jsx
@@ -1,7 +1,14 @@
+import { get, map } from 'lodash-es'
 import CartItems from '../../../components/CartItems'
 
-const CustomCartItems = () => {
-  const customItems = []
+const CustomCartItems = (props) => {
+  const { cart, selectProductMap = {} } = props
+  const items = get(cart, 'items', [])
+  const customItems = map(items, (item) => {
+    const { fish_code, quantity = 0 } = item
+    const fishName = get(selectProductMap, `${fish_code}.fish_name`, fish_code)
+    return `${fishName} x ${quantity}`
+  })
   return (
     <CartItems items={customItems} />
   )
