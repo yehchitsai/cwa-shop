c139719abe2e142afd114782665afd06d67d3d29
sky172839465 <sky172839465@gmail.com>
2024-10-16 13:52:56 +0800

    style: update drawer

--- src/components/Drawer/index.jsx
+++ src/components/Drawer/index.jsx
@@ -76,7 +76,7 @@ const Drawer = (props) => {
       </div>
       <div
         className={clx(
-          'drawer-side max-sm:flex max-sm:flex-wrap md:top-[64px] md:h-[calc(100vh-64px)] md:z-[1] max-md:z-10'
+          'drawer-side md:z-[1] max-md:z-10 md:h-[calc(100dvh-4rem)]'
         )}
       >
         {
@@ -91,44 +91,50 @@ const Drawer = (props) => {
             />
           )
         }
-        <ul
-          className={clx(
-            'menu max-sm:w-full sm:w-80 bg-base-200 p-4 text-base-content',
-            { 'h-[70%]': isBottomItemsExist },
-            { 'h-full': !isBottomItemsExist },
-            { [className]: className }
-          )}
-        >
-          {/* Sidebar content here */}
-          <li
-            className={clx(
-              'max-lg:mb-14'
-            )}
-          >
-            <label
-              htmlFor={id}
-              className={clx(
-                'btn btn-square btn-outline drawer-button absolute left-2 lg:hidden'
-              )}
-              onClick={() => setIsOpen(false)}
-            >
-              <CloseIcon size='2em' />
-            </label>
-          </li>
-          {items}
-        </ul>
-        {
-          isBottomItemsExist && (
+        <div className='menu min-h-full w-full bg-base-200 p-0'>
+          <div className='h-full grow overflow-y-scroll'>
             <ul
               className={clx(
-                'menu h-[30%] max-sm:w-full sm:w-80 bg-base-200 p-4 text-base-content relative max-lg:top-[70%] max-sm:top-auto lg:top-auto',
+                'menu w-full bg-base-200 p-4 text-base-content',
+                { 'h-[70%]': isBottomItemsExist },
+                { 'h-full': !isBottomItemsExist },
                 { [className]: className }
               )}
             >
-              {bottomItems}
+              {/* Sidebar content here */}
+              <li
+                className={clx(
+                  'max-lg:mb-14'
+                )}
+              >
+                <label
+                  htmlFor={id}
+                  className={clx(
+                    'btn btn-square btn-outline drawer-button absolute left-2 lg:hidden'
+                  )}
+                  onClick={() => setIsOpen(false)}
+                >
+                  <CloseIcon size='2em' />
+                </label>
+              </li>
+              {items}
             </ul>
-          )
-        }
+          </div>
+          {
+            isBottomItemsExist && (
+              <div className='flex-none'>
+                <ul
+                  className={clx(
+                    'menu max-sm:w-full bg-base-200 p-4 text-base-content relative max-lg:top-[70%] max-sm:top-auto lg:top-auto',
+                    { [className]: className }
+                  )}
+                >
+                  {bottomItems}
+                </ul>
+              </div>
+            )
+          }
+        </div>
       </div>
     </div>
   )
