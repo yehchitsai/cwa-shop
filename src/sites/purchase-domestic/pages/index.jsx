f9589a1c191cdbe9c5a5f456697632c5e5dbad7b
sky172839465 <sky172839465@gmail.com>
2024-10-16 11:08:14 +0800

    fix: linter error

--- src/sites/purchase-domestic/pages/index.jsx
+++ src/sites/purchase-domestic/pages/index.jsx
@@ -49,6 +49,24 @@ const PurchaseDomestic = () => {
   const { phase, phaseType } = searchMenuAction
   const isAddToCart = !(clickRowData.fish_code in selectProductMap)
 
+  const updateCart = async (newSelectProducts) => {
+    const orderItems = map(newSelectProducts, (newSelectProduct) => {
+      return pick(newSelectProduct, ['fish_code', 'quantity', 'request'])
+    })
+    const body = { order_items: orderItems }
+    const toastId = toast.loading('更新購物車...')
+    const [error, result] = await safeAwait(trigger(body))
+    if (error) {
+      purchaseModalRef.current.close()
+      toast.error(`更新購物車失敗! ${error.message}`, { id: toastId })
+      return
+    }
+
+    const newCart = get(result, 'results', initCart)
+    setCart(newCart)
+    toast.success('更新購物車成功!', { id: toastId })
+  }
+
   const onRemoveRow = (rowData) => {
     const { fish_code } = rowData
     const newSelectProducts = selectProducts.filter((selectProduct) => {
@@ -89,24 +107,6 @@ const PurchaseDomestic = () => {
     const newSelectProducts = onSelectRow(formValues)
     updateCart(newSelectProducts)
     purchaseModalRef.current.close()
-  }
-
-  const updateCart = async (newSelectProducts) => {
-    const orderItems = map(newSelectProducts, (newSelectProduct) => {
-      return pick(newSelectProduct, ['fish_code', 'quantity', 'request'])
-    })
-    const body = { order_items: orderItems }
-    const toastId = toast.loading('更新購物車...')
-    const [error, result] = await safeAwait(trigger(body))
-    if (error) {
-      purchaseModalRef.current.close()
-      toast.error(`更新購物車失敗! ${error.message}`, { id: toastId })
-      return
-    }
-
-    const newCart = get(result, 'results', initCart)
-    setCart(newCart)
-    toast.success('更新購物車成功!', { id: toastId })
   }
 
   const onPurchaseModalClose = () => {
