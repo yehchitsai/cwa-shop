c466161ff1c3e08f25652fb5375bd5238eca66b7
sky172839465 <sky172839465@gmail.com>
2025-08-16 12:24:42 +0800

    feat: add api resp

--- src/sites/staff/pages/quotation/Coupon.jsx
+++ src/sites/staff/pages/quotation/Coupon.jsx
@@ -17,6 +17,7 @@ import useCreate from '../../../../hooks/useCreate'
 import getEnvVar from '../../../../utils/getEnvVar'
 import getApiPrefix from '../../../../utils/getApiPrefix'
 import getFormValues from '../../../../utils/getFormValues'
+import safeJSON from '../../../../utils/safeJSON'
 
 const FORM = {
   EXCEL: 'excel'
@@ -35,6 +36,7 @@ const Coupon = () => {
   const { t } = useTranslation()
   const resetBtn = useRef()
   const [isExcelUploaded, setIsExcelUploaded] = useState(false)
+  const [apiResult, setApiResult] = useState({})
   const {
     trigger,
     isMutating
@@ -43,6 +45,7 @@ const Coupon = () => {
   const onDropExcels = (excelFiles) => {
     const isAcceptFile = isUndefined(get(excelFiles, '0.code')) && !isEmpty(excelFiles)
     setIsExcelUploaded(isAcceptFile)
+    setApiResult({})
   }
 
   const clearForm = () => {
@@ -60,7 +63,7 @@ const Coupon = () => {
       }
     }
     const toastId = toast.loading('Uploading...')
-    const [createError] = await safeAwait(trigger(postParams))
+    const [createError, result] = await safeAwait(trigger(postParams))
     if (createError) {
       toast.error(`Error! ${createError.message}`, { id: toastId })
       setSubmitting(false)
@@ -69,6 +72,7 @@ const Coupon = () => {
     toast.success('Finish!', { id: toastId })
     setSubmitting(false)
     clearForm()
+    setApiResult(result)
   }
 
   return (
@@ -81,45 +85,59 @@ const Coupon = () => {
     >
       {({ errors, touched }) => (
         <Form>
-          <div className='m-auto flex w-full flex-col max-lg:m-auto max-lg:max-w-2xl max-sm:min-w-full max-sm:p-4 sm:p-12 lg:max-w-5xl'>
+          <div className='m-auto flex w-full flex-col gap-4 max-lg:m-auto max-lg:max-w-2xl max-sm:min-w-full max-sm:p-4 sm:p-12 lg:max-w-5xl'>
             <div role='alert' className='alert flex text-left'>
               <MdError size='1.5em' />
               <span>優惠方案</span>
             </div>
-            <FormRow
-              label='上傳 Excel(.xlsx)'
-              error={touched[FORM.EXCEL] && errors[FORM.EXCEL]}
-              required
-            >
-              <Dropzone
-                name={FORM.EXCEL}
-                accept={ACCEPT.EXCEL}
-                disabled={isMutating || isExcelUploaded}
-                onFinish={onDropExcels}
-                isShowPreview={false}
-              />
-              {isExcelUploaded && (
-                <div className='alert alert-success my-4 flex flex-wrap'>
-                  {`按${t('newItem')}上傳 Excel`}
+            <div className='flex flex-col gap-4 md:flex-row'>
+              <div className='md:flex-1'>
+                <FormRow
+                  label='上傳 Excel(.xlsx)'
+                  error={touched[FORM.EXCEL] && errors[FORM.EXCEL]}
+                  required
+                >
+                  <Dropzone
+                    name={FORM.EXCEL}
+                    accept={ACCEPT.EXCEL}
+                    disabled={isMutating || isExcelUploaded}
+                    onFinish={onDropExcels}
+                    isShowPreview={false}
+                  />
+                  {isExcelUploaded && (
+                    <div className='alert alert-success my-4 flex flex-wrap'>
+                      {`按${t('newItem')}上傳 Excel`}
+                    </div>
+                  )}
+                </FormRow>
+                <div className='text-right'>
+                  <button
+                    ref={resetBtn}
+                    type='reset'
+                    className='hidden'
+                  >
+                    reset
+                  </button>
+                  <button
+                    type='submit'
+                    className='btn btn-outline'
+                    disabled={isMutating}
+                  >
+                    <MdAdd size='1.5em' />
+                    {`${t('newItem')}`}
+                  </button>
                 </div>
-              )}
-            </FormRow>
-            <div className='text-right'>
-              <button
-                ref={resetBtn}
-                type='reset'
-                className='hidden'
-              >
-                reset
-              </button>
-              <button
-                type='submit'
-                className='btn btn-outline'
-                disabled={isMutating}
-              >
-                <MdAdd size='1.5em' />
-                {`${t('newItem')}`}
-              </button>
+              </div>
+              <div className='alert flex w-full flex-col items-start gap-4 md:w-1/3'>
+                <div>
+                  API response
+                </div>
+                <div>
+                  <pre className='whitespace-break-spaces break-all text-left'>
+                    {safeJSON(apiResult)}
+                  </pre>
+                </div>
+              </div>
             </div>
           </div>
         </Form>
