44bcb4edfbd2ea304aeb7a9f7e05d4581aff3662
Yusong Hsu <sky172839465@gmail.com>
2025-08-17 12:33:15 +0800

    Update CountSelect.jsx

--- src/sites/purchase-domestic/pages/CountSelect.jsx
+++ src/sites/purchase-domestic/pages/CountSelect.jsx
@@ -4,20 +4,9 @@ import { times } from 'lodash-es'
 
 const CountSelect = (props) => {
   const {
-    max = 1, className, name, disabled
+    max = 1, min = 1, className, name, disabled
   } = props
 
-  // if (disabled) {
-  //   return (
-  //     <select
-  //       className={clx(
-  //         'select select-sm select-bordered w-full',
-  //         { [className]: className }
-  //       )}
-  //       disabled
-  //     />
-  //   )
-  // }
   return (
     <Field
       as='select'
@@ -29,8 +18,8 @@ const CountSelect = (props) => {
       disabled={disabled}
     >
       <option value={-1} disabled>選擇數量</option>
-      {times(max, (index) => {
-        const count = index + 1
+      {times(max - min, (index) => {
+        const count = min + index
         return (
           <option value={count} key={count}>
             {`${count} 隻`}
