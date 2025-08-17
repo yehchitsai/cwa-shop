c20719342e97312b83e4351681516716548e08ac
Yusong Hsu <sky172839465@gmail.com>
2025-08-17 12:20:36 +0800

    Update PurchaseModalTable.jsx

--- src/sites/purchase-domestic/pages/PurchaseModalTable.jsx
+++ src/sites/purchase-domestic/pages/PurchaseModalTable.jsx
@@ -83,7 +83,7 @@ const PurchaseModalTable = (props) => {
                   disabled={!editable}
                 >
                   <option value={-1} disabled>Select fish type</option>
-                  {times(inventory).map((index) => {
+                  {times(inventory - min_purchase_quantity).map((index) => {
                     const value = index + min_purchase_quantity
                     return (
                       <option value={value} key={value}>
