42082b2833092b8c259f1598e9db3477dd0963ad
sky172839465 <sky172839465@gmail.com>
2025-11-19 21:19:00 +0800

    feat: add mock apis

---
+++ src/hooks/useBettaFishSystemState.js
@@ -0,0 +1,27 @@
+import useSWR from 'swr'
+import qs from 'query-string'
+import {
+  isEmpty, isNull, omitBy
+} from 'lodash-es'
+import getEnvVar from '../utils/getEnvVar'
+import getApiPrefix from '../utils/getApiPrefix'
+
+const host = getEnvVar('VITE_AWS_COMMON_HOST')
+const subPrefix = getEnvVar('VITE_AWS_HOST_PREFIX')
+const awsHostPrefix = getApiPrefix(subPrefix)
+const useBettaFishSystemState = (params, options = {}) => {
+  const cleanParams = omitBy(params, isEmpty)
+  const isSkip = isNull(cleanParams)
+  const qsStr = isSkip ? '' : `?${qs.stringify(cleanParams)}`
+  const url = `${awsHostPrefix}/bettafishsystemstate${qsStr}`
+  const {
+    data, error, isValidating, isLoading
+  } = useSWR(() => (isSkip ? null : ({ url, host })), { suspense: false, ...options })
+  return {
+    data,
+    isLoading: isValidating || isLoading,
+    isError: error
+  }
+}
+
+export default useBettaFishSystemState
---
+++ src/hooks/useCreateRecoverData.js
@@ -0,0 +1,26 @@
+import getEnvVar from '../utils/getEnvVar'
+import getApiPrefix from '../utils/getApiPrefix'
+import useCreate from './useCreate'
+
+const host = getEnvVar('VITE_AWS_COMMON_HOST')
+const subPrefix = getEnvVar('VITE_AWS_HOST_PREFIX')
+const awsHostPrefix = getApiPrefix(subPrefix)
+const endpoint = `${awsHostPrefix}/recoveredata`
+
+const useCreateRecoverData = () => {
+  const { trigger: originTrigger, ...rest } = useCreate(host)
+
+  const trigger = (body) => {
+    return originTrigger({
+      url: endpoint,
+      body
+    })
+  }
+
+  return {
+    ...rest,
+    trigger
+  }
+}
+
+export default useCreateRecoverData
