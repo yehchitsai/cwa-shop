5cba5530274440ceffc821a6e9a048106fd6b857
sky172839465 <sky172839465@gmail.com>
2025-11-20 22:37:52 +0800

    chore: update style

--- src/sites/bettafish-staff/pages/reorder/index.jsx
+++ src/sites/bettafish-staff/pages/reorder/index.jsx
@@ -4,14 +4,14 @@ import UploadTankInfo from './UploadTankInfo'
 
 const Page = () => {
   return (
-    <div className='m-auto flex w-full flex-col gap-4 max-lg:m-auto max-lg:max-w-2xl max-sm:min-w-full max-sm:p-4 sm:p-12 lg:max-w-5xl'>
-      <div className='flex flex-col gap-2 md:flex-row'>
+    <div className='m-auto flex w-full flex-col gap-4 max-lg:m-auto max-sm:p-4 sm:p-12'>
+      <div className='flex flex-col gap-4 md:flex-row'>
         <div className='md:flex-1'>
           <UploadNewFish />
           <div className='divider my-2' />
           <UploadTankInfo />
         </div>
-        <JsonBlock className='min-w-72 max-w-fit flex-none' />
+        <JsonBlock className='min-w-72 max-w-96 flex-none' />
       </div>
     </div>
   )
