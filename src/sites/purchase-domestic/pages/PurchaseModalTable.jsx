c1044b47fcda846aa13edf229557f1fa07006dcc
Yusong Hsu <sky172839465@gmail.com>
2026-01-31 20:59:34 +0800

    Update resetForm to include quantity in values

--- src/sites/purchase-domestic/pages/PurchaseModalTable.jsx
+++ src/sites/purchase-domestic/pages/PurchaseModalTable.jsx
@@ -37,7 +37,7 @@ const PurchaseModalTable = (props) => {
   }
 
   useEffect(() => {
-    resetForm({ values: rowData })
+    resetForm({ values: { quantity, ...rowData } })
   }, [resetForm, rowData])
 
   return (
